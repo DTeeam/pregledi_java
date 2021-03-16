7d3ced4cefe0577bbbe5763c390643c650509c12
DTeeam <teamdetecnik@gmail.com>
2021-03-16 12:26:46 +0100

    Initial commit

---
+++ src/main/java/Main_panel.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main_panel {
+    private JButton button1;
+    private JPanel panel1;
+    private JLabel label1;
+
+
+    public Main_panel() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label1.setText("tsewt blockgeb");
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Checkup neki al neki");
+        frame.setContentPane(new Main_panel().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(500,600);
+        frame.setVisible(true);
+    }
+}
